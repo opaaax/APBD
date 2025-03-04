147a894ffb28a52ad0cf86fb5d3d82f9eb49bbf7
s31294 <s31294@pjwstk.edu.pl>
2025-03-04 16:49:47 +0100

    added avg function

--- CW1/CW1/Program.cs
+++ CW1/CW1/Program.cs
@@ -1,5 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+
+static double GetAvarage(int[] arr) 
+{
+    return 0;
+}
+
+int[] arr = { 1, 2, 3, 4, 5 };
+GetAvarage(arr);
