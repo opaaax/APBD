ddc65e19be201c91c238906aeceb13f9a2d79648
opax <opaaaax@gmail.com>
2025-03-06 12:54:11 +0100

    Added GetAvarage implementation

--- CW1/CW1/Program.cs
+++ CW1/CW1/Program.cs
@@ -1,10 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
+﻿
 static double GetAvarage(int[] arr) 
 {
-    return 0;
+    return (double) arr.Sum()/arr.Length;
 }
 
-int[] arr = { 1, 2, 3, 4, 5 };
-GetAvarage(arr);
+int[] arr = { 1, 2, 3, 4, 5 ,5};
+Console.WriteLine(GetAvarage(arr));
