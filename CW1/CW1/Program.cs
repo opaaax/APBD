fd4000fccf917fcb1b84de2324b3d7a0fbfd5d9a
opax <opaaaax@gmail.com>
2025-03-06 12:58:31 +0100

    Added GetMaxVaule method and its implementation

--- CW1/CW1/Program.cs
+++ CW1/CW1/Program.cs
@@ -4,5 +4,11 @@ static double GetAvarage(int[] arr)
     return (double) arr.Sum()/arr.Length;
 }
 
+static int GetMaxValue(int[] arr)
+{
+    return arr.Max();
+} 
+
 int[] arr = { 1, 2, 3, 4, 5 ,5};
 Console.WriteLine(GetAvarage(arr));
+Console.WriteLine(GetMaxValue(arr));
