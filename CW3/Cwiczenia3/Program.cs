761603047f572a9ec418262b7d89cd2d2fe77e7c
s31294 <s31294@pjwstk.edu.pl>
2025-03-18 17:01:45 +0100

    classroom work

---
+++ CW3/Cwiczenia3/Program.cs
@@ -0,0 +1,9 @@
+﻿using ConsoleApp1;
+
+Kontener kontener = new KontenerNaPlyny(0, 5, 12, 5, 0, 20);
+kontener.ZaladujKontener(10);
+Console.WriteLine(kontener.NrSeryjny);
+
+Kontener kontener2 = new KontenerNaPlyny(0, 5, 12, 5, 0, 20);
+kontener.ZaladujKontener(10);
+Console.WriteLine(kontener2.NrSeryjny);
